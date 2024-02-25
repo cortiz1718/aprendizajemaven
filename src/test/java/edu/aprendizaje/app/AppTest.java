a6b27109f0ce8b6fc05132e8caf7f50108086dbe
Carlos93 <karlos9305@gmail.com>
2024-02-25 12:39:46 -0500

    crea el proyecto base para maven

---
+++ src/test/java/edu/aprendizaje/app/AppTest.java
@@ -0,0 +1,20 @@
+package edu.aprendizaje.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
